7e20e949053937fff3035d65ab63cd23210bbbb6
wh01s17 <jor.perezo@hotmail.com>
2025-11-26 18:04:34 -0300

    fix: styles on cards

--- assets/js/best-sellers.js
+++ assets/js/best-sellers.js
@@ -6,12 +6,11 @@ document.getElementById("cards-row").innerHTML = top4
     .map(
         (p) => `
         <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <div class="card h-100 shadow-sm border-0">
+            <div id="card" class="card h-100 shadow-sm border-0">
                 <img 
                     src="${p.img}" 
                     class="card-img-top" 
-                    alt="${p.name}" 
-                    style="height: 200px; object-fit: cover;" 
+                    alt="${p.name}"
                 />
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">${p.name}</h5>
