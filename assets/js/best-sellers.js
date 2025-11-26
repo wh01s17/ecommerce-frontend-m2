86622aae778a9bf39ceac6cd4b84f93629c7ce24
wh01s17 <jor.perezo@hotmail.com>
2025-11-26 18:48:22 -0300

    refactor render cards, best-sellers and products

--- assets/js/best-sellers.js
+++ assets/js/best-sellers.js
@@ -1,34 +1,6 @@
 import { products } from "./data.js";
+import { renderCards } from "./render-cards.js";
 
 const top4 = products.toSorted((a, b) => b.rating - a.rating).slice(0, 4);
 
-document.getElementById("cards-row").innerHTML = top4
-    .map(
-        (p) => `
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-            <div id="card" class="card h-100 shadow-sm border-0">
-                <img 
-                    src="${p.img}" 
-                    class="card-img-top" 
-                    alt="${p.name}"
-                />
-                <div class="card-body d-flex flex-column">
-                    <h5 class="card-title">${p.name}</h5>
-                    <p class="card-text small text-muted flex-grow-1">${
-                        p.description
-                    }</p>
-
-                    <p class="fw-bold mb-1">$${p.price.toLocaleString(
-                        "es-CL"
-                    )}</p>
-                    <span class="badge bg-warning text-dark mb-3">⭐ ${
-                        p.rating
-                    }</span>
-
-                    <a href="#" class="btn btn-primary mt-auto">Ver más</a>
-                </div>
-            </div>
-        </div>
-    `
-    )
-    .join("");
+renderCards(top4, "#cards-row");
---
+++ assets/js/render-cards.js
@@ -0,0 +1,41 @@
+// render-cards.js
+export function renderCards(products, targetSelector) {
+    const container = document.querySelector(targetSelector);
+
+    if (!container) return;
+
+    container.innerHTML = products
+        .map(
+            (p) => `
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <div id="card" class="card h-100 shadow-sm border-0">
+                <img 
+                    src="${p.img}" 
+                    class="card-img-top" 
+                    alt="${p.name}"
+                />
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">
+                        ${p.name}
+                    </h5>
+
+                    <p class="card-text small text-muted flex-grow-1">
+                        ${p.description}
+                    </p>
+
+                    <p class="fw-bold mb-1">
+                        $${p.price.toLocaleString("es-CL")}
+                    </p>
+
+                    <span class="badge bg-warning text-dark mb-3">
+                        ⭐ ${p.rating}
+                    </span>
+
+                    <a href="#" class="btn btn-primary mt-auto">Ver más</a>
+                </div>
+            </div>
+        </div>
+    `
+        )
+        .join("");
+}
